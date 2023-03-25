c28ec65adbc8ce792d988464d9b078b6ba0ef90c
Jersonmade <119952285+Jersonmade@users.noreply.github.com>
2023-03-25 20:39:15 +0400

    Add files via upload

---
+++ Casting types in JS/index.js
@@ -0,0 +1,97 @@
+// 1 zadanie
+// const n = 18;
+// console.log(Number(n), Boolean(n), String(n))
+
+// const str = 'hello'
+// console.log(Number(str), Boolean(str), String(str))
+
+// const nameFieldChecked = true
+// console.log(Number(nameFieldChecked), Boolean(nameFieldChecked), String(nameFieldChecked))
+
+// const age = null
+// console.log(Number(age), Boolean(age), String(age))
+
+// const nAge = undefined
+// console.log(Number(nAge), Boolean(nAge), String(nAge))
+
+// const itCompany = {
+// 	name: 'The Best IT Company',
+// 	numberOfDevelopers: 500,
+// 	isProductCompany: true,
+//  };
+
+// console.log(Number(itCompany), Boolean(itCompany), String(itCompany))
+
+// const id = Symbol('id');
+// console.log(Number(id), Boolean(id), String(id))
+
+// const bigIntNumber2 = 20n;
+// console.log(Number(bigIntNumber2), Boolean(bigIntNumber2), String(bigIntNumber2))
+
+
+
+
+
+// 2 zadanie
+// console.log(Number('10'), typeof Number('10')) //явное
+
+// console.log(+'50', typeof +'50') //неявное
+
+// console.log(Number('100'), typeof Number('100')) //явное
+
+// console.log('' +1, typeof '' +1) //неявное
+
+// console.log(String(1), typeof String(1)) //явное
+
+// console.log(+'001', typeof +'001') //неявное
+
+// console.log(1 + '', typeof 1 + '') //неявное
+
+// console.log(Boolean(1), typeof Boolean(1))//явное
+
+// console.log(String(001), typeof String(001)) //явное
+
+// console.log(Number('Hello world'), typeof Number('Hello world')) //явное
+
+
+
+
+// 3 zadanie
+// console.log(String(console.log), Number(console.log), Boolean(console.log))
+
+// console.log(String({name: 'Maxim'}), Number({name: 'Maxim'}), Boolean({name: 'Maxim'}))
+
+// //console.log(String(Symbol('key')), Number(Symbol('key')), Boolean(Symbol('key')))
+
+// console.log(String(Number), Number(Number), Boolean(Number))
+
+// console.log(String(0), Number(0), Boolean(0))
+
+// console.log(String(-10), Number(-10), Boolean(-10))
+
+// console.log(String('-105'), Number('-105'), Boolean('-105'))
+
+// console.log(String(""), Number(""), Boolean(""))
+
+
+
+// 4 zadanie
+// console.log(Number(' 1 2 3 4 5')); // NaN
+
+// console.log(Number('1234 5')); // NaN
+
+// console.log(Number('12345')); // 12345
+
+// console.log(String(false)); // false
+
+// console.log(Boolean(0000000)); // false
+
+// console.log(Boolean(0.0000001)); // true
+
+// console.log(String(undefined)); // undefined
+
+// console.log(Number('100f')); // NaN
+
+// console.log(Number('100')); // 100
+
+// console.log(Number('000001')); // 1
